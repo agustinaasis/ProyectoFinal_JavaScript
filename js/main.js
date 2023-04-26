5a67e0b9f79d896dba2cec65874e57410a0dd837
Agustina Asis <agustina.asis5@gmail.com>
2023-04-26 21:46:08 +0200

    Estado inicial del proyecto

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Nombre del Usuario
+
+let nombreDeUsuario = prompt("Por favor, ingresa tu nombre");
+
+let bienvenida = "Hola " + nombreDeUsuario + " " + "¡Bienvenido!";
+
+alert(bienvenida);
+
+let numero1 = 2023;
+
+let numero2 = prompt("Por favor, ingresa tu año de nacimiento");
+
+let edadDelUsuario = numero1 - numero2;
+alert("Tu edad es " + edadDelUsuario + " " + "años");

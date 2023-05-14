019b0d62c55f29a6d54983370bc5e47c9cda046a
Agustina Asis <agustina.asis5@gmail.com>
2023-05-14 16:55:35 +0200

    Listo para la primera pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,67 +1,51 @@
-/// LUIS: En la siguiente función me pasa que si le agrego el isNaN no me dice los alert siguientes, no toma el valor y se cierra el cuadro de "alert". Otra cosa es que, al ingresas un valor no válido, el cuadro se cierra y ya no vuelve a pedir los datos, por eso es que pensé en un ciclo while como el que le sigue a este primer intento////
-
-// function saludar(){
-//     let nombreDeUsuario = (prompt("Por favor, ingresa tu nombre")).toLocaleUpperCase();
-//     let bienvenida = "Hola " + nombreDeUsuario + " " + "¡Bienvenida!"
-
-//     if(nombreDeUsuario == ""){
-//         alert("El valor ingresado no es válido")
-//     }else if(nombreDeUsuario == " "){
-//         alert("El valor ingresado no es válido")
-//         }
-//     else{
-//         alert(bienvenida);
-//     }
-// }
-// saludar();
-
-
-/// Funcion para saludar, con ciclo while para que vuelva a pedirle al usuario que ingrese su nombre en el prompt. Esta me parece la forma más adecuada de hacerlo, pero no logro ponerle el isNaN para que el usuario NO ingrese números en el prompt ////
-
 function saludar(){
-    let nombreDeUsuario = prompt("Por favor, ingresa tu nombre").toUpperCase();
-    let bienvenida = ("Hola, " + nombreDeUsuario + " " + "¡Bienvenida!")
+        let nombreDeUsuario = prompt("Por favor, ingresa tu nombre").toUpperCase();
+        let bienvenida = ("Hola " + nombreDeUsuario + " " + "¡Bienvenido!");
 
-    while((nombreDeUsuario == "") || (nombreDeUsuario==" "))
-    {
-    alert ("Ingresa un nombre válido por favor")
-    nombreDeUsuario = prompt("Por favor, ingresa tu nombre").toUpperCase();
+        while(!isNaN(nombreDeUsuario)){
+            alert("El valor ingresado no es válido, ingrese su nombre correctamente")
+            nombreDeUsuario = prompt("Ingresa tu nombre").toUpperCase();
+        }
+        while((nombreDeUsuario == "") || (nombreDeUsuario==" "))
+        {
+        alert ("Ingresa un nombre válido por favor")
+        nombreDeUsuario = prompt("Ingresa tu nombre").toUpperCase();
+        }
+        if(nombreDeUsuario = nombreDeUsuario)
+        alert (bienvenida);
     }
-    if(nombreDeUsuario = nombreDeUsuario)
-    alert (bienvenida);
-}
 saludar();
 
+function filtrarEspacio(){
+        let espacioDecorar = prompt("¿Que espacio te gustaría decorar? Ingresa el número que indique la opción elegida: 1- Comedor/Cocina 2- Habitación 3- Living 4- Baño 5- ¡Quiero ver ideas! ");
 
-// function filtrarEspacio(){
-//     let espacioDecorar = prompt("¿Que espacio te gustaría decorar? Ingresa el número que señale la opción correcta: 1- Comedor/Cocina 2- Habitación 3- Living 4- Baño 5- ¡Quiero ver ideas! ")
+        switch(espacioDecorar){
+            case "1" :
+                alert("¡Genial! En nuestra tienda encontrarás objetos únicos para decorar tu comedor.")
+                break;
+            case "2" :
+                alert("¡Gran idea! Tener la habitación de tus sueños es posible  ¡Dirígete a nuestra tienda!")
+                break;
+            case "3" :
+                alert("Personalizá tu living con objetos únicos que se adapten a tu estilo ¡Encontralos en nuestra tienda!")
+                break;
+            case "4" :
+                alert("¡Nada mejor que un cuarto de baño con estilo! Es una excelente opción agregarle distintos elementos que le aporten diseño y practicidad.")
+                break;
+            case "5":
+                alert("¡Nada mejor que inspirarse en otros espacios que aportan ideas para nuestro lugar! Te invitamos a ver nuestro sitio y esperamos encuentres lo que estás buscando.")
+                break;
+            default: alert("¡No elegiste una opción correcta!")
+            break;
+    }
+    }
+filtrarEspacio();
 
-//     switch(espacioDecorar){
-//         case "1" :
-//             return("¡Genial! En nuestra tienda encontrarás objetos únicos para decorar tu comedor.")
-//         case "2" :
-//             alert("¡Gran idea! Tener la habitación de tus sueños es posible  ¡Dirígete a nuestra tienda!")
-//             break;
-//         case "3" :
-//             alert("Personalizá tu living con objetos únicos que se adapten a tu estilo ¡Encontralos en nuestra tienda!")
-//             break;
-//         case "4" :
-//             alert("¡Nada mejor que un cuarto de baño con estilo! Es una excelente opción agregarle distintos elementos que le aporten diseño y practicidad.")
-//             break;
-//         case "5":
-//             alert("¡Nada mejor que inspirarse en otros espacios que aportan ideas para nuestro lugar! Te invitamos a ver nuestro sitio y esperamos encuentres lo que estás buscando.")
-//             break;
-//         default: alert("¡No elegiste una opción correcta!")
-//             break;
-// }
-// }
-// filtrarEspacio();
-
-// let turnosDisponibles = 5;
-// function seleccionarTurno(turnosDisponibles){
-//     for(let i = 1; i<= turnosDisponibles; i++)
-//     {
-//         console.log("Los turnos disponibles para hoy son: " + i)
-//     }
-// }
-// seleccionarTurno(turnosDisponibles);
+let turnosDisponibles = 5;
+    function seleccionarTurno(turnosDisponibles){
+        for(let i = 1; i<= turnosDisponibles; i++)
+        {
+            console.log("Los turnos disponibles para hoy son: " + i)
+        }
+    }
+    seleccionarTurno(turnosDisponibles);

d923c9ee824167c429e21b08726c1e56dfb0a022
Agustina Asis <agustina.asis5@gmail.com>
2023-05-22 15:46:46 +0200

    Elimino comillas de los números en los cases como corrección de primera entrega

--- js/main.js
+++ js/main.js
@@ -17,28 +17,29 @@ function saludar(){
 saludar();
 
 function filtrarEspacio(){
-        let espacioDecorar = prompt("¿Que espacio te gustaría decorar? Ingresa el número que indique la opción elegida: 1- Comedor/Cocina 2- Habitación 3- Living 4- Baño 5- ¡Quiero ver ideas! ");
+        let espacioDecorar = parseInt(prompt("¿Que espacio te gustaría decorar? Ingresa el número que indique la opción elegida: 1- Comedor/Cocina 2- Habitación 3- Living 4- Baño 5- ¡Quiero ver ideas! "));
 
         switch(espacioDecorar){
-            case "1" :
+            case  1:
                 alert("¡Genial! En nuestra tienda encontrarás objetos únicos para decorar tu comedor.")
                 break;
-            case "2" :
+            case  2:
                 alert("¡Gran idea! Tener la habitación de tus sueños es posible  ¡Dirígete a nuestra tienda!")
                 break;
-            case "3" :
+            case  3:
                 alert("Personalizá tu living con objetos únicos que se adapten a tu estilo ¡Encontralos en nuestra tienda!")
                 break;
-            case "4" :
+            case  4:
                 alert("¡Nada mejor que un cuarto de baño con estilo! Es una excelente opción agregarle distintos elementos que le aporten diseño y practicidad.")
                 break;
-            case "5":
+            case 5:
                 alert("¡Nada mejor que inspirarse en otros espacios que aportan ideas para nuestro lugar! Te invitamos a ver nuestro sitio y esperamos encuentres lo que estás buscando.")
                 break;
             default: alert("¡No elegiste una opción correcta!")
             break;
     }
     }
+    
 filtrarEspacio();
 
 let turnosDisponibles = 5;

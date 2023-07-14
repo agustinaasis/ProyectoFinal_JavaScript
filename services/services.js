42ba82f5bc44da6c2ede7d3470fff00eb0dc2a2c
luigi-coder <luigicoder28@gmail.com>
2023-07-14 14:52:54 -0300

    Se mantuvo como estaba

--- services/services.js
+++ services/services.js
@@ -1,19 +1,12 @@
-// import { filtrosCategorias } from '../js/submenu.js';
-
-
 async function obtenerProductos() {
 
-    const respuesta = await fetch (('././assets/bbdd.json'))
-    
+    const respuesta = await fetch('././assets/bbdd.json');
+
     const productos = await respuesta.json();
-    
+
     return productos;
-    
-    
-    }
 
-    // filtrosCategorias();
-
+}
 
 export { obtenerProductos };
 
